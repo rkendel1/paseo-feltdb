colonelpanic8/paseo

feat(composer): prompt stash — cmd+S saves the draft to a per-provider queue
Pressing cmd+S / ctrl+S snapshots the in-progress composer (text +
attachments) into a persisted per-provider stash queue and clears the
composer, with an Undo toast. cmd+S on an empty composer (or clicking
the bookmark badge on the composer's shoulder) opens a picker anchored
above the input: arrows/Enter restore, hover X deletes, Esc closes.
Restoring appends to existing text, dedupes attachments, and removes
the entry — a queue, not a library.

- New prompt-stash zustand store (AsyncStorage persist, 20 entries per
  provider queue, oldest evicted) with pure state logic + unit tests.
- Stash attachment ids feed the draft-store attachment GC, which now
  awaits stash hydration so a startup GC pass cannot delete blobs only
  the stash references.
- New "stash" message-input keyboard action wired through the shortcut
  registry (rebindable, listed in shortcuts help, preventDefault keeps
  the browser save dialog closed).
- Picker reuses Combobox (desktop popover + native bottom sheet).

Ported from pingdotgg/t3code#4453, adapted to Paseo's attachment
metadata model (no base64/compression layer needed) and keybinding
registry.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

packages/app/src/composer/index.tsx
@@ -92,6 +92,7 @@ import type { KeyboardActionDefinition } from "@/keyboard/keyboard-action-dispat
 import type { MessageInputKeyboardActionKind } from "@/keyboard/actions";
 import { submitAgentInput } from "@/composer/submit";
 import { ComposerKeyboardScopeProvider } from "@/composer/keyboard-scope";
+import { ComposerStash } from "@/composer/stash";
 import { useAppSettings } from "@/hooks/use-settings";
 import { isWeb, isNative } from "@/constants/platform";
 import type { ForgeSearchItem } from "@getpaseo/protocol/messages";
@@ -2021,6 +2022,13 @@ export function Composer({
 
   const messageInputContainerRef = useRef<View>(null);
 
+  // Stash queues are scoped per provider: the live agent's provider when one
+  // exists, otherwise the draft flow's selected provider.
+  const stashProvider =
+    resolveAgentControlsMode(agentControls) === "draft" && agentControls
+      ? agentControls.selectedProvider
+      : agentState.provider;
+
   const isSubmitBusy =
     isProcessing ||
     isSubmitLoading ||
@@ -2061,6 +2069,16 @@ export function Composer({
             {sendErrorNode}
 
             <View ref={messageInputContainerRef} style={styles.messageInputContainer}>
+              <ComposerStash
+                provider={stashProvider}
+                anchorRef={messageInputContainerRef}
+                userInput={userInput}
+                setUserInput={setUserInput}
+                attachments={attachments}
+                setAttachments={setSelectedAttachments}
+                disabled={isComposerLocked || isSubmitBusy}
+                focusInput={focusInput}
+              />
               <AutocompletePopover
                 visible={autocompleteVisible}
                 anchorRef={messageInputContainerRef}

rkendel1/paseo-feltdb

packages/app/src/composer/index.tsx
@@ -103,6 +103,7 @@ import type { MessageInputKeyboardActionKind } from "@/keyboard/actions";
 import { submitAgentInput } from "@/composer/submit";
 import { createMessageSubmissionWriter } from "@/composer/submission/writer";
 import { ComposerKeyboardScopeProvider, useComposerKeyboardScope } from "@/composer/keyboard-scope";
+import { ComposerStash } from "@/composer/stash";
 import { useAppSettings } from "@/hooks/use-settings";
 import { RenderProfile } from "@/utils/render-profiler";
 import { AfterPaintPublication } from "@/composer/after-paint-publication";
@@ -2202,6 +2203,13 @@ function ComposerContentImpl({
 
   const messageInputContainerRef = useRef<View>(null);
 
+  // Stash queues are scoped per provider: the live agent's provider when one
+  // exists, otherwise the draft flow's selected provider.
+  const stashProvider =
+    resolveAgentControlsMode(agentControls) === "draft" && agentControls
+      ? agentControls.selectedProvider
+      : agentState.provider;
+
   const isSubmitLoadingVisible =
     isProcessing || isSubmitLoading || isUploadingFile || pendingNativeImagePastes > 0;
   const isSubmitDisabled =
@@ -2256,6 +2264,16 @@ function ComposerContentImpl({
             {sendErrorNode}
 
             <View ref={messageInputContainerRef} style={styles.messageInputContainer}>
+              <ComposerStash
+                provider={stashProvider}
+                anchorRef={messageInputContainerRef}
+                userInput={userInput}
+                setUserInput={replaceUserInput}
+                attachments={attachments}
+                setAttachments={setSelectedAttachments}
+                disabled={isComposerLocked || isSubmitDisabled}
+                focusInput={focusInput}
+              />
               <ComposerAutocomplete
                 visible={autocompleteVisible}
                 anchorRef={messageInputContainerRef}
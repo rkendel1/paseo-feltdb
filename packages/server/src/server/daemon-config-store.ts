colonelpanic8/paseo

Support editing and usage for provider accounts

packages/server/src/server/daemon-config-store.ts
@@ -22,6 +22,7 @@ interface LoggerLike {
 
 export interface DaemonConfigChangeDetails {
   removedProviders: readonly string[];
+  replacedProviders: readonly string[];
 }
 
 type ConfigListener = (config: MutableDaemonConfig, details: DaemonConfigChangeDetails) => void;
@@ -195,9 +196,24 @@ export class DaemonConfigStore {
         "Relay is controlled by a daemon launch override. Remove PASEO_RELAY_ENABLED or the relay CLI flag before changing it here.",
       );
     }
-    const { removeProviders = [], ...configPatch } = parsedPatch;
+    const { removeProviders = [], replaceProviders = {}, ...configPatch } = parsedPatch;
     const removedProviders = Array.from(new Set(removeProviders));
-    const merged = deepMerge(this.current, configPatch);
+    const replacedProviders = Object.keys(replaceProviders);
+    const removedProviderSet = new Set(removedProviders);
+    const conflictingProvider = replacedProviders.find((providerId) =>
+      removedProviderSet.has(providerId),
+    );
+    if (conflictingProvider) {
+      throw new Error(`Provider ${conflictingProvider} cannot be removed and replaced together`);
+    }
+    const mergePatch =
+      replacedProviders.length > 0
+        ? {
+            ...configPatch,
+            providers: { ...configPatch.providers, ...replaceProviders },
+          }
+        : configPatch;
+    const merged = deepMerge(omitProvidersFromConfig(this.current, replacedProviders), mergePatch);
     const next = MutableDaemonConfigSchema.parse(
       omitMetadataGenerationProvidersFromConfig(
         omitProvidersFromConfig(merged, removedProviders),
@@ -210,12 +226,18 @@ export class DaemonConfigStore {
     });
     const configChanged = !isEqualValue(this.current, next);
 
-    if (!configChanged && removedProviders.length === 0) {
+    if (!configChanged && removedProviders.length === 0 && replacedProviders.length === 0) {
       return this.current;
     }
 
-    const persistedBeforePatch = this.persistConfig(next, removedProviders);
-    if (!configChanged) return this.current;
+    const persistedBeforePatch = this.persistConfig(next, removedProviders, replacedProviders);
+    if (!configChanged) {
+      const changeDetails: DaemonConfigChangeDetails = { removedProviders, replacedProviders };
+      for (const listener of this.changeListeners) {
+        listener(next, changeDetails);
+      }
+      return this.current;
+    }
 
     const previous = this.current;
     const appliedFieldChanges: AppliedFieldChange[] = [];
@@ -242,7 +264,7 @@ export class DaemonConfigStore {
       throw error;
     }
 
-    const changeDetails: DaemonConfigChangeDetails = { removedProviders };
+    const changeDetails: DaemonConfigChangeDetails = { removedProviders, replacedProviders };
     for (const listener of this.changeListeners) {
       listener(next, changeDetails);
     }
@@ -277,12 +299,14 @@ export class DaemonConfigStore {
   private persistConfig(
     config: MutableDaemonConfig,
     removeProviders: readonly string[],
+    replaceProviders: readonly string[],
   ): PersistedConfig {
     const persisted = loadPersistedConfig(this.paseoHome, this.logger);
     const nextPersisted = mergeMutableConfigIntoPersistedConfig({
       persisted,
       mutable: config,
       removeProviders,
+      replaceProviders,
       persistRelayEnabled: this.relayEnabledMutable,
     });
     savePersistedConfig(this.paseoHome, nextPersisted, this.logger);
@@ -294,9 +318,10 @@ function mergeMutableConfigIntoPersistedConfig(params: {
   persisted: PersistedConfig;
   mutable: MutableDaemonConfig;
   removeProviders: readonly string[];
+  replaceProviders: readonly string[];
   persistRelayEnabled: boolean;
 }): PersistedConfig {
-  const { persisted, mutable, removeProviders, persistRelayEnabled } = params;
+  const { persisted, mutable, removeProviders, replaceProviders, persistRelayEnabled } = params;
   if (!mutable.relay) {
     throw new Error("Mutable daemon config is missing relay state");
   }
@@ -304,7 +329,7 @@ function mergeMutableConfigIntoPersistedConfig(params: {
   const metadataGenerationProviders = readMetadataGenerationProviders(mutable);
   const persistedProviderOverrides = omitProvidersFromOverrides(
     persisted.agents?.providers as Record<string, ProviderOverride> | undefined,
-    removeProviders,
+    [...removeProviders, ...replaceProviders],
   );
   const providerOverrides = applyMutableProviderConfigToOverrides(
     persistedProviderOverrides,

rkendel1/paseo-feltdb

packages/server/src/server/daemon-config-store.ts
@@ -22,6 +22,7 @@ interface SupportedMutableConfigPatch {
   browserTools?: { enabled?: boolean };
   providers?: MutableDaemonConfig["providers"];
   removeProviders?: string[];
+  replaceProviders?: MutableDaemonConfigPatch["replaceProviders"];
   metadataGeneration?: MutableDaemonConfig["metadataGeneration"];
   autoArchiveAfterMerge?: boolean;
   enableTerminalAgentHooks?: boolean;
@@ -40,6 +41,7 @@ interface LoggerLike {
 
 export interface DaemonConfigChangeDetails {
   removedProviders: readonly string[];
+  replacedProviders: readonly string[];
 }
 
 export interface DaemonConfigReloadResult {
@@ -260,6 +262,7 @@ function pickSupportedPatchFields(patch: MutableDaemonConfigPatch): SupportedMut
       : {}),
     ...(patch.providers !== undefined ? { providers: patch.providers } : {}),
     ...(patch.removeProviders !== undefined ? { removeProviders: patch.removeProviders } : {}),
+    ...(patch.replaceProviders !== undefined ? { replaceProviders: patch.replaceProviders } : {}),
     ...(patch.metadataGeneration?.providers !== undefined
       ? { metadataGeneration: { providers: patch.metadataGeneration.providers } }
       : {}),
@@ -351,9 +354,24 @@ export class DaemonConfigStore {
         "Relay is controlled by a daemon launch override. Remove PASEO_RELAY_ENABLED or the relay CLI flag before changing it here.",
       );
     }
-    const { removeProviders = [], ...configPatch } = parsedPatch;
+    const { removeProviders = [], replaceProviders = {}, ...configPatch } = parsedPatch;
     const removedProviders = Array.from(new Set(removeProviders));
-    const merged = deepMerge(this.current, configPatch);
+    const replacedProviders = Object.keys(replaceProviders);
+    const removedProviderSet = new Set(removedProviders);
+    const conflictingProvider = replacedProviders.find((providerId) =>
+      removedProviderSet.has(providerId),
+    );
+    if (conflictingProvider) {
+      throw new Error(`Provider ${conflictingProvider} cannot be removed and replaced together`);
+    }
+    const mergePatch =
+      replacedProviders.length > 0
+        ? {
+            ...configPatch,
+            providers: { ...configPatch.providers, ...replaceProviders },
+          }
+        : configPatch;
+    const merged = deepMerge(omitProvidersFromConfig(this.current, replacedProviders), mergePatch);
     if (parsedPatch.skills?.selection !== undefined) {
       merged.skills = { selection: parsedPatch.skills.selection };
     }
@@ -367,21 +385,17 @@ export class DaemonConfigStore {
 
     const configChanged = !isEqualValue(this.current, next);
 
-    if (!configChanged && removedProviders.length === 0) {
+    if (!configChanged && removedProviders.length === 0 && replacedProviders.length === 0) {
       return this.current;
     }
 
     const { previous: persistedBeforePatch, knownNext } = this.persistConfig(
-      configPatch,
+      mergePatch,
       removedProviders,
+      replacedProviders,
     );
-    if (!configChanged) {
-      this.lastKnownPersisted = knownNext;
-      return this.current;
-    }
-
     try {
-      this.applyReplacement(next, { removedProviders });
+      this.applyReplacement(next, { removedProviders, replacedProviders });
       this.lastKnownPersisted = knownNext;
     } catch (error) {
       savePersistedConfig(this.paseoHome, persistedBeforePatch, this.logger);
@@ -436,7 +450,7 @@ export class DaemonConfigStore {
     const removedProviders = Object.keys(this.current.providers).filter(
       (provider) => !(provider in desired.providers),
     );
-    this.applyReplacement(desired, { removedProviders });
+    this.applyReplacement(desired, { removedProviders, replacedProviders: [] });
     this.lastKnownPersisted = persisted;
 
     return {
@@ -453,7 +467,13 @@ export class DaemonConfigStore {
     const changedFieldPaths = Array.from(this.fieldChangeHandlers.keys()).filter((path) => {
       return !isEqualValue(getValueAtPath(this.current, path), getValueAtPath(next, path));
     });
-    if (isEqualValue(this.current, next) && changeDetails.removedProviders.length === 0) return;
+    if (
+      isEqualValue(this.current, next) &&
+      changeDetails.removedProviders.length === 0 &&
+      changeDetails.replacedProviders.length === 0
+    ) {
+      return;
+    }
 
     const previous = this.current;
     const appliedFieldChanges: AppliedFieldChange[] = [];
@@ -547,8 +567,9 @@ export class DaemonConfigStore {
   }
 
   private persistConfig(
-    patch: Omit<SupportedMutableConfigPatch, "removeProviders">,
+    patch: Omit<SupportedMutableConfigPatch, "removeProviders" | "replaceProviders">,
     removeProviders: readonly string[],
+    replaceProviders: readonly string[],
   ): { previous: PersistedConfig; knownNext: PersistedConfig } {
     const persisted = loadPersistedConfig(this.paseoHome, this.logger);
     const merge = (source: PersistedConfig) =>
@@ -556,6 +577,7 @@ export class DaemonConfigStore {
         persisted: source,
         patch,
         removeProviders,
+        replaceProviders,
         persistRelayEnabled: this.relayEnabledMutable,
       });
     const nextPersisted = merge(persisted);
@@ -567,13 +589,14 @@ export class DaemonConfigStore {
 
 function mergeMutablePatchIntoPersistedConfig(params: {
   persisted: PersistedConfig;
-  patch: Omit<SupportedMutableConfigPatch, "removeProviders">;
+  patch: Omit<SupportedMutableConfigPatch, "removeProviders" | "replaceProviders">;
   removeProviders: readonly string[];
+  replaceProviders: readonly string[];
   persistRelayEnabled: boolean;
 }): PersistedConfig {
-  const { persisted, patch, removeProviders, persistRelayEnabled } = params;
+  const { persisted, patch, removeProviders, replaceProviders, persistRelayEnabled } = params;
   const daemon = mergeMutableDaemonPatch(persisted.daemon, patch, persistRelayEnabled);
-  const agents = mergeMutableAgentPatch(persisted.agents, patch, removeProviders);
+  const agents = mergeMutableAgentPatch(persisted.agents, patch, removeProviders, replaceProviders);
   return {
     ...persisted,
     ...(patch.pluginsEnabled !== undefined ? { pluginsEnabled: patch.pluginsEnabled } : {}),
@@ -585,14 +608,16 @@ function mergeMutablePatchIntoPersistedConfig(params: {
 
 function mergeMutableAgentPatch(
   persistedAgents: PersistedConfig["agents"],
-  patch: Omit<SupportedMutableConfigPatch, "removeProviders">,
+  patch: Omit<SupportedMutableConfigPatch, "removeProviders" | "replaceProviders">,
   removeProviders: readonly string[],
+  replaceProviders: readonly string[],
 ): PersistedConfig["agents"] {
   if (
     patch.providers === undefined &&
     patch.metadataGeneration === undefined &&
     patch.skills === undefined &&
-    removeProviders.length === 0
+    removeProviders.length === 0 &&
+    replaceProviders.length === 0
   ) {
     return persistedAgents;
   }
@@ -600,7 +625,7 @@ function mergeMutableAgentPatch(
   const next = { ...persistedAgents } as Record<string, unknown>;
   const persistedProviderOverrides = omitProvidersFromOverrides(
     persistedAgents?.providers as Record<string, ProviderOverride> | undefined,
-    removeProviders,
+    [...removeProviders, ...replaceProviders],
   );
   const providerOverrides = applyMutableProviderConfigToOverrides(
     persistedProviderOverrides,
@@ -629,7 +654,7 @@ function mergeMutableAgentPatch(
 
 function mergeMutableDaemonPatch(
   persistedDaemon: PersistedConfig["daemon"],
-  patch: Omit<SupportedMutableConfigPatch, "removeProviders">,
+  patch: Omit<SupportedMutableConfigPatch, "removeProviders" | "replaceProviders">,
   persistRelayEnabled: boolean,
 ): PersistedConfig["daemon"] {
   const next = { ...persistedDaemon } as NonNullable<PersistedConfig["daemon"]>;